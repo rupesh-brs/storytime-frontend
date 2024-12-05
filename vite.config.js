e6ec196fda92dbc3196b5887839f61bf1088754f
rupesh-brs <alajangirupesh@gmail.com>
2024-12-05 08:45:30 +0530

    adding

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+dotenv.config();  // Load .env variables
+
 export default defineConfig({
   plugins: [react(), svgr()],
-  server:{
-    port: process.env.port || 3000,
+  server: {
+    port: process.env.VITE_PORT || 3000
   }
 })
