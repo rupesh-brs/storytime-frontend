3abbdc9421c4741213e74b237b565e19c66a7e89
rupesh-brs <alajangirupesh@gmail.com>
2024-12-05 08:56:25 +0530

    adding

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
 import dotenv from "dotenv";
 
 dotenv.config();  // Load .env variables
 
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from "vite-plugin-svgr";
+
+
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
-    port: process.env.VITE_PORT || 3000
-  }
-})
+    port: import.meta.env.VITE_PORT || 3000,
+    host: "0.0.0.0",
+  },
+});
